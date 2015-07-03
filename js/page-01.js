fb688fbb282e33284b97775168e08f28a133a33c
Simon Zou <simon.zou@gmail.com>
2015-07-02 21:30:24 -0600

    added process_state_data to page-01.js

--- js/page-01.js
+++ js/page-01.js
@@ -22,3 +22,28 @@ function main() {
 		draw_map('percent-map', state_aggregate_data, 'percent')
 	})
 }
+
+function process_state_data(data) {
+	var old_mpg_sum = 0;
+	var new_mpg_sum = 0;
+	var cost = 0;
+
+	var aggregate_data = {};
+	_.each(data, function (datum) {
+		old_mpg_sum += Number.parseInt(datum[OLD_CAR_MPG]);
+		new_mpg_sum += Number.parseInt(datum[NEW_CAR_MPG]);
+		cost += Number.parseInt(datum[INVOICE_AMOUNT]);
+	});
+
+	var mpg_difference = new_mpg_sum - old_mpg_sum; 
+	var percentage_improvement = mpg_difference / old_mpg_sum;
+	var cost_per_mpg = cost / mpg_difference;
+
+	aggregate_data = {
+		percent: percentage_improvement,
+		percent_display: format_percent(percentage_improvement),
+		cost_per_mpg: cost_per_mpg,
+		cost_display: format_money(cost_per_mpg)
+	}
+	return aggregate_data;
+}
